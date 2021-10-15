411f9c1a691e9fe510483f04ab0328fdeaa122c5
leonardocaselli <leocaselli@hotmail.com>
2021-10-14 21:24:53 -0300

    first commit instalacion limpia

---
+++ Express-Api/routes/index.js
@@ -0,0 +1,9 @@
+const { Router } = require('express')
+
+const router = Router()
+
+/* Rutas principales */
+//router.use('/jokes', require('./jokes'))
+//router.use('/auth', require('./auth'))
+
+module.exports = router
---
+++ README.md
@@ -0,0 +1,1 @@
+"# React-Express_template" 
